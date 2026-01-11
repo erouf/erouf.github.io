b79a6171e106baa257aa718062d3d4a86837a5ea
erouf <119777681+erouf@users.noreply.github.com>
2026-01-11 04:24:54 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-  // This ensures assets load correctly on GitHub Pages later
-  base: '/', 
-})
